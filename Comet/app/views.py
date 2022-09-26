e482c784c63112030c4e37867a0471b72381674a
Wolfskee <63683607+Wolfskee@users.noreply.github.com>
2022-09-25 20:34:37 -0500

    1.0
    hompage已经做好暂无sql

---
+++ Comet/app/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+def homePage(req):
+    #搜索框所抓取的搜索词条，存为searchIte
+    print(req.POST)  #查看post方法所传回来的数据
+
+    searchItem = req.POST.get("searchItem")
+    print(searchItem)
+
+    return render(req, "Homepage.html")
