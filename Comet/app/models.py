d331471aea33e6660646a531efbd296343e771e0
Wolfskee <63683607+Wolfskee@users.noreply.github.com>
2022-10-08 13:19:35 -0500

    database 搭建完成

--- Comet/app/models.py
+++ Comet/app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+#新建table 命名为Costco， 在数据库里名字实际显示为app_Costco
+class Costco(models.Model):
+    name = models.CharField(max_length=32)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    name = models.CharField(max_length=32)
+
+#新建table 命名为Costco， 在数据库里名字实际显示为app_Walmart
+class Walmart(models.Model):
+    name = models.CharField(max_length=32)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    name = models.CharField(max_length=32)
--- Comet/app/views.py
+++ Comet/app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def homePage(req):
-    #搜索框所抓取的搜索词条，存为searchIte
+    #搜索框所抓取的搜索词条，存为searchItem
     print(req.POST)  #查看post方法所传回来的数据
 
     searchItem = req.POST.get("searchItem")
