6e153379df8f4e6d52c7a01a55392f4a1aa48df7
Wolfskee <63683607+Wolfskee@users.noreply.github.com>
2022-10-26 16:07:53 -0500

    最新版
    可从database里取出数据并展示在网页上

--- Comet/app/models.py
+++ Comet/app/models.py
@@ -1,13 +1,24 @@
 from django.db import models
-
 #新建table 命名为Costco， 在数据库里名字实际显示为app_Costco
-class Costco(models.Model):
-    name = models.CharField(max_length=32)
+class AppCostco(models.Model):
+    id = models.BigAutoField(primary_key=True)
     price = models.DecimalField(max_digits=8, decimal_places=2)
     name = models.CharField(max_length=32)
 
-#新建table 命名为Costco， 在数据库里名字实际显示为app_Walmart
-class Walmart(models.Model):
-    name = models.CharField(max_length=32)
+    class Meta:
+        managed = False
+        db_table = 'app_costco'
+
+
+class AppWalmart(models.Model):
+    id = models.BigAutoField(primary_key=True)
     price = models.DecimalField(max_digits=8, decimal_places=2)
     name = models.CharField(max_length=32)
+
+    class Meta:
+        managed = False
+        db_table = 'app_walmart'
+
+
+
+
